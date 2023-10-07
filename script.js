d0e427774d38e06ec0c5900eb174e6c20220a17a
sspccreates <145820126+sspccreates@users.noreply.github.com>
2023-10-06 22:27:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
-const students = {
-  "5928000256": { name: "Max Weller", status: "Not updated" },
-  "5928000268": { name: "Charlotte Collier", status: "Not updated" },
-  "5928000267": { name: "Mason Collier", status: "Not updated" }
-};
-
 function showStudentInfo() {
-  console.log('Submit button clicked!'); // Log a message to check if the function is called
   const userId = document.getElementById('userId').value;
-  console.log('User ID:', userId); // Log the user ID to check if it's retrieved correctly
-  const student = students[userId];
 
-  if (student) {
-    document.getElementById('studentName').innerText = `Student: ${student.name}`;
-    document.getElementById('status').innerText = `Status: ${student.status}`;
-    document.getElementById('studentInfo').style.display = 'block';
-  } else {
-    alert('Invalid User ID. Please try again.');
-  }
+  // Assuming that each student has a separate HTML file with their user ID as the filename
+  const studentPageUrl = `user/${userId}.html`;
+  window.location.href = studentPageUrl;
 }
 
 function updateStatus(action) {
-  const userId = document.getElementById('userId').value;
-  const student = students[userId];
-
-  if (student) {
-    student.status = `Updated status for ${student.name}: ${action}`;
-    document.getElementById('status').innerText = `Status: ${student.status}`;
-  } else {
-    alert('Invalid User ID. Please try again.');
-  }
+  console.log('Status updated:', action);
+  // Implement logic to update the status here
+  // This function can be customized to update the student's status as needed
 }
