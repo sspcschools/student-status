5442dacca6923e0f8ee51496a590746656f26445
sspccreates <145820126+sspccreates@users.noreply.github.com>
2023-10-06 22:10:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 const students = [
-  { name: "Max Weller", status: "Not updated" },
+  { name: "Max Weller", status: "Not updated", periods: {} },
   // Add more students here
-  // { name: "Student Name", status: "Not updated" }
+  // { name: "Student Name", status: "Not updated", periods: {} }
 ];
 
 let currentStudentIndex = 0;
 
-function updateStatus() {
+function updateStatus(action) {
   const student = students[currentStudentIndex];
-  const newStatus = `Updated status for ${student.name}`;
+  const newStatus = `Updated status for ${student.name}: ${action}`;
   document.getElementById('status').innerText = newStatus;
+  student.status = newStatus;
 
-  // Move to the next student for the next button click
-  currentStudentIndex = (currentStudentIndex + 1) % students.length;
-  document.getElementById('studentName').innerText = students[currentStudentIndex].name;
+  if (action === 'Bathroom') {
+    // Display the bathroom popup
+    document.getElementById('popup').style.display = 'block';
+  }
+}
+
+function updateBathroomStatus() {
+  const period = document.getElementById('period').value;
+  const student = students[currentStudentIndex];
+
+  if (!student.periods[period]) {
+    student.periods[period] = { bathroom: 1 };
+  } else {
+    student.periods[period].bathroom++;
+  }
+
+  closePopup();
+  updateStatus(`Student used bathroom for Period ${period}`);
+}
+
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+}
+
+function showStudentInfo(index) {
+  currentStudentIndex = index;
+  const student = students[index];
+  document.getElementById('studentName').innerText = student.name;
+  document.getElementById('status').innerText = student.status;
 }
