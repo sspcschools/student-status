b3cc119cfe634afddd2d1b8efe0130c05cacb6fd
sspccreates <145820126+sspccreates@users.noreply.github.com>
2023-10-06 22:22:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,30 @@
-const students = [
-  { name: "Max Weller", status: "Not updated", periods: {}, userId: "5928000256" },
-  { name: "Charlotte Collier", status: "Not updated", periods: {}, userId: "5928000268" },
-  { name: "Mason Collier", status: "Not updated", periods: {}, userId: "5928000267" }
-];
-
-let currentStudentIndex = 0;
-
-function updateStatus(action) {
-  const student = students[currentStudentIndex];
-  const newStatus = `Updated status for ${student.name}: ${action}`;
-  document.getElementById('status').innerText = newStatus;
-  student.status = newStatus;
-
-  if (action === 'Bathroom') {
-    // Display the bathroom popup
-    document.getElementById('popup').style.display = 'block';
-  }
-}
-
-function updateBathroomStatus() {
-  const period = document.getElementById('period').value;
-  const student = students[currentStudentIndex];
-
-  if (!student.periods[period]) {
-    student.periods[period] = { bathroom: 1 };
-  } else {
-    student.periods[period].bathroom++;
-  }
-
-  closePopup();
-  updateStatus(`Student used bathroom for Period ${period}`);
-}
-
-function closePopup() {
-  document.getElementById('popup').style.display = 'none';
-}
+const students = {
+  "5928000256": { name: "Max Weller", status: "Not updated" },
+  "5928000268": { name: "Charlotte Collier", status: "Not updated" },
+  "5928000267": { name: "Mason Collier", status: "Not updated" }
+};
 
 function showStudentInfo() {
-  const studentSelect = document.getElementById('studentSelect');
-  currentStudentIndex = parseInt(studentSelect.value);
-  const student = students[currentStudentIndex];
-  document.getElementById('studentName').innerText = `Student: ${student.name}`;
-  document.getElementById('status').innerText = student.status;
+  const userId = document.getElementById('userId').value;
+  const student = students[userId];
+
+  if (student) {
+    document.getElementById('studentName').innerText = `Student: ${student.name}`;
+    document.getElementById('status').innerText = `Status: ${student.status}`;
+    document.getElementById('studentInfo').style.display = 'block';
+  } else {
+    alert('Invalid User ID. Please try again.');
+  }
+}
+
+function updateStatus(action) {
+  const userId = document.getElementById('userId').value;
+  const student = students[userId];
+
+  if (student) {
+    student.status = `Updated status for ${student.name}: ${action}`;
+    document.getElementById('status').innerText = `Status: ${student.status}`;
+  } else {
+    alert('Invalid User ID. Please try again.');
+  }
 }
